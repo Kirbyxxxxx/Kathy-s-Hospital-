179e63ab55cffad8c61ddf9d697d61d4b1731b96
Kirbyxxxxx <katherine.heryanto@gmail.com>
2026-06-27 16:24:32 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -168,9 +168,9 @@ with c3:
   
   col_temp. col_hr = st.column(2)
   with col_temp:
-    temperature_level = st.selectbox("Temperature", options=list(dur_map.keys()))
+    temperature_level = st.selectbox("Temperature", options=list(dur_map.keys())), index=1
     with col_hr:
-      heart_rate_level = st.selectbox("Heart Rate", options=list(hr_map.keys()))
+      heart_rate_level = st.selectbox("Heart Rate", options=list(hr_map.keys())), index==1
   #Section 4 - Medical History
   st.markdown("""
   <div style = "backgound:#f0fdf4;border:1px solid #bb7d0;border-radius:14px;
@@ -201,7 +201,7 @@ with c3:
   """, unsafe_allow_html=True)
   col_age, col_gen = st.columns(2)
   with col_age:
-    age = st.number_input("Age", min_value = 1,max_value = 100 ,value 35)
+    age = st.number_input("Age", min_value = 1,max_value = 100, value 35)
   with col_gen:
     gender = st.selectbox("Gender", options=['Female','Male']
   
