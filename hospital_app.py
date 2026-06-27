5359cc4a5795d29dac98839a58809909806b91dd
Kirbyxxxxx <katherine.heryanto@gmail.com>
2026-06-27 16:56:57 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -151,7 +151,7 @@ with st.form("triage_form"):
     
     col_cc, col_dur = st.columns(2)
     with col_cc:
-      chief_complant = st.selectbox("Chief Complaint", options=list(cc_map.keys()))
+      chief_complaint = st.selectbox("Chief Complaint", options=list(cc_map.keys()))
     with col_dur:
       duration = st.selectbox("Duration", options=list(dur_map.keys()))
     #Section 3 - Severity
