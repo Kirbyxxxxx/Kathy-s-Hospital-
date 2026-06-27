d81d4619e3b8d00e49ddfa053f695f3987254567
Kirbyxxxxx <katherine.heryanto@gmail.com>
2026-06-27 16:30:17 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -169,8 +169,8 @@ with c3:
   col_temp. col_hr = st.column(2)
   with col_temp:
     temperature_level = st.selectbox("Temperature", options=list(temp_map.keys()), index=1
-    with col_hr:
-      heart_rate_level = st.selectbox("Heart Rate", options=list(hr_map.keys()), index==1
+  with col_hr:
+    heart_rate_level = st.selectbox("Heart Rate", options=list(hr_map.keys()), index==1
       
   #Section 4 - Medical History
   st.markdown("""
