c6db275a3cb395ebcfbc7fc9d13ce670aa112d2f
Kirbyxxxxx <katherine.heryanto@gmail.com>
2026-06-27 14:42:44 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -5,7 +5,7 @@ import pickle
 import os
 
 st.set_page_config(
-  page_title="Smart Hospital Patient Navigator,
+  page_title="Smart Hospital Patient Navigator",
   page_icon="🏥",
   layout="wide",
 )
